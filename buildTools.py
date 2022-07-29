cfb3df246088650dde61dce62e2fe4f5e2024dee
yijin840 <2900086967@qq.com>
2022-07-29 18:44:45 +0800

    change buildTools

--- buildTools.py
+++ buildTools.py
@@ -1,15 +1,23 @@
 import os
 
 upath = os.path.expanduser('~')
-# print(upath)
-mdir = upath + "\.tools"
 
-spath = "setx path " + mdir + " /m"
-# cmd = "mkdir " + mdir
-# setx "Path" ""
-print(spath)
-print("设置环境变量ing ......")
-os.system(spath)
-print("设置环境变量完毕")
+mdir = upath + "\.tools"
+a = os.getenv("path")
+
+# print(a)
+spath = "set path \"" + os.getenv("path") + ";" + mdir + "\" /m"
+# spath = "setx path \"" + os.getenv("path") + ";" + mdir + "\" /m"
+cmd = "mkdir " + mdir
+s = os.getenv("Path") + ";" + mdir
+print(s)
+os.putenv("path", "%s"%s)
+# print(mdir)
+print()
+print(os.getenv("path"))
+commands.getoutput('java -version')
+# print("设置环境变量ing ......")
+# os.system(spath)
+# print("设置环境变量完毕")
 
 # os.system(cmd)
