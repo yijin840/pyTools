44d07090dab59c83300061dc2691962d543e16ec
yijin840 <2900086967@qq.com>
2022-08-01 20:39:59 +0800

    temp

--- buildTools.py
+++ buildTools.py
@@ -5,19 +5,11 @@ upath = os.path.expanduser('~')
 mdir = upath + "\.tools"
 a = os.getenv("path")
 
-# print(a)
-spath = "set path \"" + os.getenv("path") + ";" + mdir + "\" /m"
-# spath = "setx path \"" + os.getenv("path") + ";" + mdir + "\" /m"
-cmd = "mkdir " + mdir
-s = os.getenv("Path") + ";" + mdir
-print(s)
-os.putenv("path", "%s"%s)
-# print(mdir)
-print()
-print(os.getenv("path"))
-commands.getoutput('java -version')
-# print("设置环境变量ing ......")
-# os.system(spath)
-# print("设置环境变量完毕")
+def get_path():
+    return os.getenv("path")
+def set_path():
+    os.environ["path"] = mdir + ";" + a
 
-# os.system(cmd)
+if __name__ == "__main__":
+    cur_path = get_path()
+    print(cur_path)
