577333891d8d221905b3409975a0fd4e80b957dc
yijin840 <2900086967@qq.com>
2022-08-04 14:45:38 +0800

    add change git msg bash

---
+++ git/changeGitMsg.sh
@@ -0,0 +1,14 @@
+#!/usr/bin/env bash
+
+OLD_MAIL=""
+NEW_MAIL=""
+OLD_NAME=""
+git filter-branch --commit-filter '
+        if [ "$GIT_AUTHOR_EMAIL" != "<$OLD_MAIL>" ];
+        then
+                GIT_AUTHOR_NAME="<$NEW_NAME>";
+                GIT_AUTHOR_EMAIL="<$NEW_MAIL>";
+                git commit-tree "$@";
+        else
+                git commit-tree "$@";
+        fi' HEAD
