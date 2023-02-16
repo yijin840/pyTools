3b9a961e230f578b01ba7ea1d7d0b0f998346014
yijin840 <2900086967@qq.com>
2023-02-16 16:21:40 +0800

    采用装饰器优化代码结构

--- ssh/local_ssh.py
+++ ssh/local_ssh.py
@@ -6,36 +6,41 @@ import re
 import time
 from paramiko.channel import ChannelStdinFile
 
-
 class ssh_client(object):
     _ssh = paramiko.SSHClient()
     _java_path = ""
     _channel = any
-
-    _name = ""
-    _host = ""
-    _port = ""
-    _password = ""
     # 初始化
-
     def __init__(self, host, port, user, password, name):
-        self._ssh.load_system_host_keys()
-        self._ssh.set_missing_host_key_policy(paramiko.AutoAddPolicy())
-        self._ssh.connect(hostname=host, port=port,
-                          username=user, password=password)
         self._name = name
         self._host = host
         self._port = port
+        self._user = user
         self._password = password
-        self._channel = self._ssh.get_transport().open_session()
-        self._java_path = self.find_java_path()
+        self._ssh.load_system_host_keys()
+        self._ssh.set_missing_host_key_policy(paramiko.AutoAddPolicy())
+
+    # 在类里定义一个装饰器
+    def clothes(func):
+        def ware(self, *args, **kwargs):
+            self._ssh.connect(hostname=self._host, port=self._port,
+                            username=self._user, password=self._password)
+            self._channel = self._ssh.get_transport().open_session()
+            return func(self, *args, **kwargs)
+        return ware
 
     # 设置Java路径
-    def set_java_path(self, path):
-        self._java_path = path
+    def set_java_home(self, path=""):
+        if path != None and path != "":  
+            self._java_home = path
+        if self._java_path == "" or self._java_path == None:
+                self._java_path = self.find_java_path()
+                self._java_home = self._java_path[0:-12] + "bin"
 
     # 查找Java路径
     def find_java_path(self, cmd="bash -lc 'which java'"):
+        if self._java_path != "" and self._java_path != None:
+            return self._java_path
         virtual_path = self.execute_cmd(cmd, get_pty=True, isPrint=False)
         fd = virtual_path[0].find("->")
         if fd != -1:
@@ -55,7 +60,7 @@ class ssh_client(object):
         4. get_pty: 是否开启新的终端，一般用于top这种需要持续输出的命令
         5. isPrint: 是否打印输出,默认打印
     '''
-
+    @clothes
     def execute_cmd(self, main_cmd, vl=[], hl=[], get_pty=False, isPrint=True, isPw=False):
         if main_cmd == "" or main_cmd == None:
             return
@@ -75,7 +80,6 @@ class ssh_client(object):
     '''
         1. 根据端口号查看进程号
     '''
-
     def find_pid_by_port(self, port):
         cmd = "lsof -i:%s | awk -F = 'NR==2{print $1}' | tr -s ' '|cut -d' ' -f2" % str(
             port)
@@ -84,9 +88,7 @@ class ssh_client(object):
     '''
         1.执行top命令
     '''
-
     def execute_top_mem(self):
-
         stdin, stdout, stderr = self._ssh.exec_command(
             "top | grep -i mem", get_pty=True)
         self.std_print(stdin, stdout, stderr)
@@ -94,26 +96,24 @@ class ssh_client(object):
     '''
         1. jps查看java进程，需要设置java环境，因为可能存在多种Java环境
     '''
-
     def java_jps(self):
-        cmd = "%s/jps" % self._java_path
+        print(self._java_home)
+        cmd = "%s/jps" % self._java_home
         return self.execute_cmd(cmd)
 
     '''
         1. jmap查看java内存对象，需要设置java环境，因为可能存在多种Java环境
     '''
-
     def java_jmap(self, pid):
-        cmd = "%s/jmap -heap " % self._java_path + str(pid)
+        cmd = "%s/jmap -heap " % self._java_home + str(pid)
         return self.execute_cmd(cmd)
 
     '''
         1. jmap查看jvm参数数据，需要设置java环境，因为可能存在多种Java环境
         2. 默认异步输出，不能停止，谨慎使用
     '''
-
     def java_jstat_sync(self, pid):
-        cmd = "%s/jstat -gcutil %s 5000 100" % str(self._java_path,
+        cmd = "%s/jstat -gcutil %s 5000 100" % str(self._java_home,
                                                    pid)
         print("  %s     %s     %s      %s      %s     %s    %s     %s    %s    %s     %s   \n'" % ('幸存1区当前使用比例', '幸存2区当前使用比例',
                                                                                                    '伊甸园区使用比例', '老年代使用比例', '元数据区使用比例', '压缩使用比例', '年轻代垃圾回收次数', '年轻代垃圾回收消耗时间', '老年代垃圾回收次数', '老年代垃圾回收消耗时间', '垃圾回收消耗总时间'))
@@ -122,7 +122,6 @@ class ssh_client(object):
     '''
     进度条
     '''
-
     def progress(self, filename, size, sent):
         sys.stdout.write("%s\'s progress: %.2f%%   \r" %
                          (filename, float(sent)/float(size)*100))
@@ -133,7 +132,6 @@ class ssh_client(object):
         4. stderr 错误输出
         5. isPrint 是否打印输出
     '''
-
     def std_print(self, stdin: ChannelStdinFile, stdout: ChannelStdinFile, stderr: ChannelStdinFile, isPrint=True):
         stdout._set_mode("rb")
         stderr._set_mode("rb")
@@ -158,7 +156,7 @@ class ssh_client(object):
      1. get True 是 从服务器拿
      2. get False 是 本地发送到服务器 
     '''
-
+    @clothes
     def copy_file(self, origin, target, get=True):
         print("copy file ", origin, " to ", target)
         scp = SCPClient(self._ssh.get_transport(), progress=self.progress)
@@ -177,7 +175,6 @@ class ssh_client(object):
     l      符号链接
     s      套接字
     '''
-
     def find(self, file_name, type=None, dir="~", deep_path=None):
         cmd = "find % s " % dir
         if type != None:
@@ -191,7 +188,6 @@ class ssh_client(object):
         nohup执行命令，默认没有输出，需要指定日志路径
         1. 默认日志路径是回收站，即不会保存日志文件
     '''
-
     def nohup_execute(self, cmd, log_path="/dev/null"):
         cmd = "bash -lc 'nohup %s > %s 2>&1 & '" % (cmd, log_path)
         return self.execute_cmd(cmd, isPrint=False)
@@ -202,7 +198,6 @@ class ssh_client(object):
         3. jtl_path jtl文件路径 , 报告文件
         4. jmeter_path jmeter路径 jmeter路径
     '''
-
     def run_jmeter(self, jmx, jtl_path, jmeter_path):
         cmd = "sh %s -n -t %s -l %s " % (jmeter_path, jmx, jtl_path)
         return self.nohup_execute(cmd)
@@ -213,7 +208,6 @@ class ssh_client(object):
         3. port 端口号， 默认会停止port进程，谨慎使用
         4. dir 部署路径
     '''
-
     def deploy_jar_project(self, jar, port, dir="~"):
         self.copy_file(jar, dir)
         # 端口必须得要，不然没办法启动起来
