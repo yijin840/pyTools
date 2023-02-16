61003518285ae00f9e4e95015366c2ec8fc249b7
yijin840 <2900086967@qq.com>
2023-02-16 14:09:25 +0800

    优化代码实现逻辑

--- ssh/local_ssh.py
+++ ssh/local_ssh.py
@@ -2,24 +2,38 @@
 import paramiko
 from scp import SCPClient
 import sys
+import re
+import time
+from paramiko.channel import ChannelStdinFile
 
 
 class ssh_client(object):
     _ssh = paramiko.SSHClient()
     _java_path = ""
+    _channel = any
 
-    #初始化
-    def __init__(self, host, port, user, password):
+    _name = ""
+    _host = ""
+    _port = ""
+    _password = ""
+    # 初始化
+
+    def __init__(self, host, port, user, password, name):
         self._ssh.load_system_host_keys()
         self._ssh.set_missing_host_key_policy(paramiko.AutoAddPolicy())
         self._ssh.connect(hostname=host, port=port,
                           username=user, password=password)
+        self._name = name
+        self._host = host
+        self._port = port
+        self._password = password
+        self._channel = self._ssh.get_transport().open_session()
 
-    #设置Java路径
+    # 设置Java路径
     def set_java_path(self, path):
         self._java_path = path
-        
-    #查找Java路径
+
+    # 查找Java路径
     def find_java_path(self):
         cmd = "bash -lc 'which java'"
         virtual_path = self.execute_cmd(cmd, get_pty=True, isPrint=False)
@@ -35,7 +49,8 @@ class ssh_client(object):
         5. sync: 是否同步输出,一般用于top这种需要持续输出的命令，如果为True，get_pty必须为True，不会停止输出，必须手动结束程序
         6. isPrint: 是否打印输出,默认打印
     '''
-    def execute_cmd(self, main_cmd, vl=[], hl=[], get_pty=False, sync=False, isPrint=True):
+
+    def execute_cmd(self, main_cmd, vl=[], hl=[], get_pty=False, isPrint=True, isPw=False):
         if len(vl) != 0:
             for v in vl:
                 main_cmd = main_cmd + " | " + v
@@ -44,14 +59,15 @@ class ssh_client(object):
                 main_cmd = main_cmd + " && " + h
         stdin, stdout, stderr = self._ssh.exec_command(
             main_cmd, get_pty=get_pty)
-        if sync:
-            self.std_print_sync(stdin, stdout, stderr)
-        else:
-            return self.std_print(stdin, stdout, stderr, isPrint)
+        if isPw:
+            stdin.write(self._password + "\n")
+            stdin.flush()
+        return self.std_print(stdin, stdout, stderr, isPrint)
 
     '''
         1. 根据端口号查看进程号
     '''
+
     def find_pid_by_port(self, port):
         cmd = "lsof -i:%s | awk -F = 'NR==2{print $1}' | tr -s ' '|cut -d' ' -f2" % str(
             port)
@@ -60,7 +76,9 @@ class ssh_client(object):
     '''
         1.执行top命令
     '''
+
     def execute_top_mem(self):
+
         stdin, stdout, stderr = self._ssh.exec_command(
             "top | grep -i mem", get_pty=True)
         self.std_print(stdin, stdout, stderr)
@@ -68,6 +86,7 @@ class ssh_client(object):
     '''
         1. jps查看java进程，需要设置java环境，因为可能存在多种Java环境
     '''
+
     def java_jps(self):
         cmd = "%s/jps" % self._java_path
         return self.execute_cmd(cmd)
@@ -75,6 +94,7 @@ class ssh_client(object):
     '''
         1. jmap查看java内存对象，需要设置java环境，因为可能存在多种Java环境
     '''
+
     def java_jmap(self, pid):
         cmd = "%s/jmap -heap " % self._java_path + str(pid)
         return self.execute_cmd(cmd)
@@ -83,6 +103,7 @@ class ssh_client(object):
         1. jmap查看jvm参数数据，需要设置java环境，因为可能存在多种Java环境
         2. 默认异步输出，不能停止，谨慎使用
     '''
+
     def java_jstat_sync(self, pid):
         cmd = "%s/jstat -gcutil %s 5000 100" % str(self._java_path,
                                                    pid)
@@ -93,6 +114,7 @@ class ssh_client(object):
     '''
     进度条
     '''
+
     def progress(self, filename, size, sent):
         sys.stdout.write("%s\'s progress: %.2f%%   \r" %
                          (filename, float(sent)/float(size)*100))
@@ -103,47 +125,39 @@ class ssh_client(object):
         4. stderr 错误输出
         5. isPrint 是否打印输出
     '''
-    def std_print(self, stdin, stdout, stderr, isPrint=True):
+
+    def std_print(self, stdin: ChannelStdinFile, stdout: ChannelStdinFile, stderr: ChannelStdinFile, isPrint=True):
         stdout._set_mode("rb")
         stderr._set_mode("rb")
-        result_stdout = stdout.readlines()
-        result_stderr = stderr.readlines()
-
         res = []
-        for i in result_stdout:
-            if isPrint:
-                print(i.decode('utf-8', errors="ignore"), end="")
-            res.append(str(i.decode('utf-8', errors="ignore")[:-1]))
-        for i in result_stderr:
-            if isPrint:
-                print(i.decode('utf-8', errors="ignore"), end="")
-        return res
-
-    '''
-        1. 异步打印输出，不中断
-        2. stdin 输入
-        3. stdout 输出
-        4. stderr 错误输出
-    '''
-    def std_print_sync(self, stdin, stdout, stderr):
-        stdout._set_mode("rb")
-        stderr._set_mode("rb")
         for line in iter(lambda: stdout.readline(2048), ""):
-            print(line.decode('utf-8', errors="ignore"), end="")
+            if isPrint:
+                print(line.decode('utf-8', errors="ignore"), end="")
+            res.append(str(line.decode('utf-8', errors="ignore")[:-1]))
+            stdout.flush()
             if line == "" or stdout.channel.exit_status_ready():
                 break
+            
+        for line in iter(lambda: stderr.readline(2048), ""):
+            if isPrint:
+                print(line.decode('utf-8', errors="ignore"), end="")
+            stderr.flush()
+            if line == "" or stderr.channel.exit_status_ready():
+                break
+        return res
 
     '''
      1. get True 是 从服务器拿
      2. get False 是 本地发送到服务器 
     '''
+
     def copy_file(self, origin, target, get=True):
         print("copy file ", origin, " to ", target)
         scp = SCPClient(self._ssh.get_transport(), progress=self.progress)
         if get:
             scp.get(remote_path=origin, local_path=target)
         else:
-            scp.put(files=origin, target=target)
+            scp.put(files=origin, remote_path=target)
         scp.close()
 
     '''
@@ -155,6 +169,7 @@ class ssh_client(object):
     l      符号链接
     s      套接字
     '''
+
     def find(self, file_name, type=None, dir="~", deep_path=None):
         cmd = "find % s " % dir
         if type != None:
@@ -168,9 +183,9 @@ class ssh_client(object):
         nohup执行命令，默认没有输出，需要指定日志路径
         1. 默认日志路径是回收站，即不会保存日志文件
     '''
+
     def nohup_execute(self, cmd, log_path="/dev/null"):
-        if log_path != None:
-            cmd = "nohup %s > %s 2>&1 &" % (cmd, log_path)
+        cmd = "bash -lc 'nohup %s > %s 2>&1 & '" % (cmd, log_path)
         return self.execute_cmd(cmd, isPrint=False)
 
     '''
@@ -179,9 +194,10 @@ class ssh_client(object):
         3. jtl_path jtl文件路径 , 报告文件
         4. jmeter_path jmeter路径 jmeter路径
     '''
+
     def run_jmeter(self, jmx, jtl_path, jmeter_path):
-        cmd = "jmeter -n -t %s -l %s" % (jmx, jtl_path)
-        self.nohup_execute(cmd, jmeter_path)
+        cmd = "sh %s -n -t %s -l %s " % (jmeter_path, jmx, jtl_path)
+        return self.nohup_execute(cmd)
 
     '''
         1. 部署jar包到服务器
@@ -189,6 +205,7 @@ class ssh_client(object):
         3. port 端口号， 默认会停止port进程，谨慎使用
         4. dir 部署路径
     '''
+
     def deploy_jar_project(self, jar, port, dir="~"):
         self.copy_file(jar, dir)
         # 端口必须得要，不然没办法启动起来
