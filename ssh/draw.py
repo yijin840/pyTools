5e5b725fad6996748f9cbe4696d29382ebd4b765
yijin840 <2900086967@qq.com>
2023-02-23 10:31:34 +0800

    add subplot

--- ssh/draw.py
+++ ssh/draw.py
@@ -1,20 +1,38 @@
-# -*- coding:utf-8 -*-
-
+from pandas.plotting import register_matplotlib_converters
 import matplotlib.pyplot as plt
-import time
-import numpy as np
-from numpy.random import rand
-
+from matplotlib.ticker import MultipleLocator
 
 plt.rcParams['font.sans-serif'] = [u'SimHei']
 plt.rcParams['axes.unicode_minus'] = False
-fig = plt.figure()
-fig.canvas.manager.set_window_title("内存占用分析")
-def draw(val):
-    plt.plot(val)
-    plt.ylabel('some numbers')
+
+# 子图画布方式画图
+
+
+def draw(x_data, y_data, title="", xlable="", ylable=""):
+    # 准备画布,设置属性
+    figure = plt.figure(title)
+    axes = figure.add_subplot(1, 1, 1)
+    axes.set_title(title)
+    axes.set_ylabel(ylable)
+    axes.set_xlabel(xlable)
+    # 稀疏Y轴
+    ymajorLocator = MultipleLocator(10000)
+    axes.yaxis.set_major_locator(ymajorLocator)
+    # 画折线
+    axes.plot(x_data, y_data, color='tab:blue')
+    # 显示
     plt.show()
-def main():
-    lis = [1,2,3,4,5]
-    draw(lis)
-main()
+
+# 原始画图方式
+
+
+def plt_draw(x_data, y_data, title="", xlable="", ylable=""):
+    plt.cla()
+    # 准备绘制数据
+    # "g" 表示红色，marksize用来设置'D'菱形的大小
+    plt.plot(x_data, y_data, "g", marker='D', markersize=2)
+    # 绘制坐标轴标签
+    plt.xlabel(xlable)
+    plt.ylabel(ylable)
+    plt.title(title)
+    plt.pause(0.1)
